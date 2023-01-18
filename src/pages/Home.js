2ebf1cdfe35fefe1608966b74860f0c27cb070bc
NishantPotdar <99239063+NishantPotdar@users.noreply.github.com>
2023-01-18 18:14:18 +0530

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import { getDocs, collection, deleteDoc,updateDoc, doc } from "firebase/firestore";
+import { auth, db } from "../firebase-config";
+
+function Home({ isAuth }) {
+    const [postLists, setPostList] = useState([]);
+    const postsCollectionRef = collection(db, "posts");
+
+    const updatePost = async (id) => {
+      const postDoc = doc(db, "posts", id);
+      await updateDoc(postDoc);
+    };
+
+    const deletePost = async (id) => {
+      const postDoc = doc(db, "posts", id);
+      await deleteDoc(postDoc);
+    };
+
+      useEffect(() => {
+      const getPosts = async () => {
+        const data = await getDocs(postsCollectionRef)
+        setPostList(data.docs.map ((doc) => ({ ...doc.data(), id: doc.id })));
+      };
+  
+      getPosts();
+    }, [updatePost]);
+
+    useEffect(() => {
+      const getPosts = async () => {
+        const data = await getDocs(postsCollectionRef)
+        setPostList(data.docs.map ((doc) => ({ ...doc.data(), id: doc.id })));
+      };
+  
+      getPosts();
+    }, [deletePost]);
+  
+    return (
+      <div className="homePage">
+        {postLists.map((post) => {
+          return (
+            <div className="post" key={post?.id}>
+              <div className="postHeader">
+                <div className="title">
+                  <h1> {post?.title}</h1>
+                </div>
+               
+                <div className="deletePost">
+                  {isAuth && post?.author?.id === auth.currentUser.uid && (
+                    <button
+                      onClick={() => {
+                        deletePost(post?.id);
+                      }}>
+                      &#128465;
+                    </button>
+                  )}
+                </div>
+
+                <div className="updatePost">
+                  {isAuth && post?.author?.id === auth.currentUser.uid && (
+                    <button onClick={() => {
+                      updatePost(post?.id);
+                    }}>
+                      Edit
+                    </button>
+                  )}
+                </div>
+                
+              </div>
+              <div className="postTextContainer"> {post?.postText} </div>
+              <h3>@{post?.author?.name}</h3>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+  
+
+export default Home;
